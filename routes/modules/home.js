3271a1dd03fb80bfbc046935b2fda2f2e120781c
ccj777 <002ntou@gmail.com>
2022-08-08 21:52:15 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -15,25 +15,65 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// 搜尋功能
+// 搜尋功能包含&排序功能
 router.get('/search', (req, res) => {
+
   const keywords = req.query.keywords
   const keyword = keywords.trim().toLowerCase()
+  const sort = req.query.sort
+  let sortMode = {}
 
-  if (!req.query.keywords) {
-    res.redirect("/")
+  // 用於控制index.hbs的下拉選單中<option>之selected屬性
+  let optionSelected = {
+    asc: false,
+    desc: false,
+    category: false,
+    location: false,
+  }
+
+  // 不同排序方式設定
+  switch (sort) {
+    case 'asc':
+      sortMode = { name: "asc" }
+      optionSelected.asc = true
+      break
+    case 'desc':
+      sortMode = { name: "desc" }
+      optionSelected.desc = true
+      break
+    case 'category':
+      sortMode = { category: "asc" }
+      optionSelected.category = true
+      break
+    case 'location':
+      sortMode = { location: "asc" }
+      optionSelected.location = true
+      break
+  }
+
+  // 未輸入關鍵字時
+  if (!keywords) {
+    return Restaurant.find()
+      .lean()
+      .sort(sortMode)
+      .then(restaurantsData => {
+        res.render("index", { restaurantsData: restaurantsData, optionSelected })
+      })
+      .catch(error => console.log(error))
   }
 
   Restaurant.find()
     .lean()
+    .sort(sortMode)
     .then(restaurantsData => {
-      const filteredRestaurantsData = restaurantsData.filter(data =>
-        data.name.toLowerCase().includes(keyword)
-      )
-      res.render("index", { restaurantsData: filteredRestaurantsData, keywords })
+      const filteredRestaurantsData = restaurantsData.filter(data => data.name.toLowerCase().includes(keyword))
+      res.render("index", { restaurantsData: filteredRestaurantsData, keywords, optionSelected })
     })
     .catch(error => console.log(error))
 })
+
+
+
 
 // 匯出路由模組
 module.exports = router
