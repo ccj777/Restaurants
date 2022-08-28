a88a23ea8af421feeff866e36b919c82d68e000f
ccj777 <002ntou@gmail.com>
2022-08-29 04:43:05 +0800

    feat: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,15 +1,40 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../Restaurant')
+const User = require('../user')
+
 const restaurantList = require('../../restaurant.json').results
+const userList = require('../../user.json').results
+
 const db = require("../../config/mongoose")
 
 db.once('open', () => {
-  console.log("run seeder script")
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log('restaurantSeeder done.')
-      db.close
+  Promise.all(
+    Array.from(userList, (userlist) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(userlist.password, salt))
+        .then(hash => User.create({
+          name: userlist.name,
+          email: userlist.email,
+          password: hash
+        })
+        )
+        .then(user => {
+          const userId = user._id
+          const seedRestaurant = []
+          userlist.restaurant_index.forEach(index => {
+            restaurantList[index].userId = userId
+            seedRestaurant.push(restaurantList[index])
+          })
+          return Restaurant.create(seedRestaurant)
+        })
     })
-    .catch(error => {
-      console.log(error)
+  )
+    .then(() => {
+      console.log('user & restaurant seeders loaded!')
+      process.exit()
     })
 })
